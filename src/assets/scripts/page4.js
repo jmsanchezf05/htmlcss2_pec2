3d48acbe53cf4b1de1be428e3835ad7ba8a04df2
jmsanchezf05 <jmsanchezf05@uoc.edu>
2022-05-03 20:27:16 +0200

    testing reservas

--- src/assets/scripts/page4.js
+++ src/assets/scripts/page4.js
@@ -3,9 +3,12 @@ let inputNombre;
 let spanNombreError;
 let inputApellidos;
 let spanApellidosError;
+let inputFecha;
+let spanFechaError;
+let inputCheck;
 let boton;
-let validaAsiento = false;
 let asiento = -1;
+let asientos = [];
 
 let arrayReservas = [];
 
@@ -18,16 +21,29 @@ class asientoClass {
 
 
 class reservasClass {
-    constructor(_nombre, _apellidos, _butaca) {
+    constructor(_nombre, _apellidos, _butacas, _fecha) {
         this.nombre = _nombre;
         this.apellidos = _apellidos;
-        this.butaca = _butaca;
+        this.butacas = _butacas;
+        this.fecha = _fecha;
     }
-    setButaca(numero) {
-        this.butaca = numero;
+    /*   setButaca(numero) {
+           this.butaca = numero;
+       }*/
+    getNombre() {
+        return this.nombre;
+    }
+    getApellidos() {
+        return this.apellidos;
+    }
+    getAsientos() {
+        return this.butacas;
+    }
+    getFecha() {
+        return this.fecha;
     }
     getAll() {
-        return "{ nombre:" + this.nombre + " apellidos:" + this.apellidos + " butaca:" + this.butaca + "}";
+        return "{ nombre:" + this.nombre + " apellidos:" + this.apellidos + " butaca:" + this.butacas + "}";
     }
 }
 
@@ -38,28 +54,40 @@ const atiendeEventoButaca = (evento) => {
 
     htmlElemento.classList.toggle("seleccionado");
 
-    if (asiento === -1) {
-        asiento = htmlElemento.getAttribute('num');
-        validaAsiento = true;
+    const nuevoAsiento = htmlElemento.getAttribute('num');
+
+    if (nuevoAsiento != asientos[asientos.length - 1]) {
+        asiento = nuevoAsiento;
+        asientos.push(nuevoAsiento);
     } else {
         asiento = -1;
-        validaAsiento = false;
+        asientos.pop(); //Remove
     }
 
-    console.log("elemento lista:", htmlElemento.classList, "asiento" + asiento);
+    // console.log("elemento lista:", htmlElemento.classList, "asiento", asiento, "asientos:" + asientos);
 }
 
+const validaAsiento = () => {
+    const result = false;
+    if (asientos.length > 0) {
+        result = true;
+    }
+    return result;
+}
 
 const validaNombre = () => {
     const textoNombre = inputNombre.value;
     const patron = /^.{3,15}$/;
     if (patron.test(textoNombre)) {
         spanNombreError.innerHTML = "OK";
-        spanNombreError.classList.toogle("ok");
+        spanNombreError.classList.add("ok");
+        spanNombreError.classList.remove("fail");
+        //spanNombreError.classList.toogle("ok");
         return true;
     } else {
         spanNombreError.innerHTML = "Debe cumplir una longitud de entre 3 y 15 caracteres";
-        spanNombreError.classList.toogle("fail");
+        spanNombreError.classList.add("fail");
+        spanNombreError.classList.remove("ok");
         return false;
     }
 }
@@ -70,27 +98,112 @@ const validaApellidos = () => {
 
     if (patron.test(textoApellido)) {
         spanApellidosError.innerHTML = "OK";
+        spanApellidosError.classList.add("ok");
+        spanApellidosError.classList.remove("fail");
         return true;
     } else {
+        spanApellidosError.classList.add("fail");
+        spanApellidosError.classList.remove("ok");
         spanApellidosError.innerHTML = "Debe cumplir una longitud de entre 3 y 35 caracteres";
         return false;
     }
 }
 
+const validaFecha = () => {
+    const result = false;
+    const textFecha = inputFecha.value;
+
+    console.log("textfecah", textFecha);
+
+    if (textFecha !== "") {
+        spanFechaError.classList.add("ok");
+        spanFechaError.classList.remove("fail");
+        spanFechaError.innerHTML = "OK"
+        result = true;
+    } else {
+        spanFechaError.classList.add("fail");
+        spanFechaError.classList.remove("ok");
+        spanFechaError.innerHTML = "Debe elegir una fecha";
+        return result;
+    }
+}
+
+const validaCheck = () => {
+    const result = false;
+    console.log("CHECK", inputCheck.checked);
+
+    if (inputCheck.checked == true) {
+        result = true;
+    }
+    return result;
+}
 
 const salvaDatos = (evento) => {
-    if (validaNombre() && validaApellidos() && validaAsiento) {
+    if (validaNombre() && validaApellidos() && validaAsiento() && validaFecha() && validaCheck()) {
         alert("Su reserva se ha realizado correctamente");
-        const reserva = new reservasClass(inputNombre.value, inputApellidos.value, asiento);
+
+        const reserva = new reservasClass(inputNombre.value, inputApellidos.value, asientos, inputFecha.value);
         arrayReservas.push(reserva);
         asiento.className += "reservado";
         console.log("reserva OK", reserva.getAll());
-        console.log(" Reservas actuales:", arrayReservas);
+        mostrarDatosEnDocument(reserva);
 
     } else {
         alert("Su reserva no se ha podido realizar. Compruebe los datos.");
     }
 }
+
+
+const mostrarDatosEnDocument = (reserv) => {
+    console.log("recibo reserva", reserv);
+    const puntoInsercion = document.getElementById("datos");
+    puntoInsercion.innerHTML = "";
+
+
+    let div = document.createElement("div");
+    div.classList.add("datosDeReserva");
+
+    let tag0 = document.createElement("p");
+    let text0 = document.createTextNode("DATOS DE LA RESERVA");
+    tag0.classList.add("tituloReserva");
+    tag0.appendChild(text0);
+    div.appendChild(tag0);
+
+    let tag = document.createElement("p");
+    let text = document.createTextNode("Nombre: " + reserv.getNombre());
+    tag.appendChild(text);
+    div.appendChild(tag);
+
+    let tag1 = document.createElement("p");
+    let text1 = document.createTextNode("Apellidos: " + reserv.getApellidos());
+    tag1.appendChild(text1);
+    div.appendChild(tag1);
+
+
+    console.log("asientosMostrar:", reserv.getAsientos());
+
+    const butacas = reserv.getAsientos();
+    if (butacas != null) {
+        let tag2 = document.createElement("p");
+        let totalButacas = "";
+        for (let i = 0; i < butacas.length; i++) {
+            totalButacas += butacas[i] + " , ";
+        }
+        let text2 = document.createTextNode("Asiento: " + totalButacas);
+        tag2.appendChild(text2);
+        div.appendChild(tag2);
+
+    }
+
+    let tag3 = document.createElement("p");
+    let text3 = document.createTextNode("Fecha: " + reserv.getFecha());
+    tag3.appendChild(text3);
+    div.appendChild(tag3);
+
+    puntoInsercion.appendChild(div);
+}
+
+
 
 window.onload = function () {
     boton = document.getElementById("boton");
@@ -110,4 +223,11 @@ window.onload = function () {
 
     spanNombreError = document.getElementById("nombreError");
     spanApellidosError = document.getElementById("apellidosError");
-};
+
+    inputFecha = document.getElementById("fecha");
+    inputFecha.addEventListener("blur", validaFecha);
+    spanFechaError = document.getElementById("fechaError");
+
+    inputCheck = document.getElementById("check");
+    //inputCheck.addEventListener("change", () => console.log("check:" + inputCheck.checked));
+}
